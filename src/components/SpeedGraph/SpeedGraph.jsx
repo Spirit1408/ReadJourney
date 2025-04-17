027aab9eeaa33c9d66576044c45695fba62c3450
Oleh Shalashov <spirit14081992@gmail.com>
2025-04-17 19:48:43 -0300

    Update logic and styles for graphic component, add fix for Diary styles

--- src/components/SpeedGraph/SpeedGraph.jsx
+++ src/components/SpeedGraph/SpeedGraph.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import css from "./SpeedGraph.module.css";
 
-const SpeedGraph = ({ speed, width = 43, height = 17 }) => {
+const SpeedGraph = ({ speed }) => {
+	const baseWidth = 43;
+	const baseHeight = 17;
+
 	const normalizedSpeed = Math.min(Math.max(speed / 60, 0), 1);
 
-	const svgRatio = width / 43;
-
-	const startX = 5 * svgRatio;
-	const endX = 38 * svgRatio;
-	const bottomY = 14 * svgRatio;
-
-	const startY = 8 * svgRatio;
-	const endY = startY - normalizedSpeed * 8 * svgRatio;
+	const startX = 5;
+	const endX = 38;
+	const bottomY = 14;
+	const startY = 8;
+	const endY = startY - normalizedSpeed * 8;
 
 	const points = [
 		`${startX},${bottomY}`,
@@ -21,25 +21,25 @@ const SpeedGraph = ({ speed, width = 43, height = 17 }) => {
 	].join(" ");
 
 	return (
-		<svg
-			className={css.graph}
-			width="100%"
-			height="100%"
-			viewBox={`0 0 ${width} ${height}`}
-			preserveAspectRatio="xMidYMid meet"
-		>
-			<polygon points={points} fill="#30B94D" fillOpacity="0.2" />
+		<div className={css.graphContainer}>
+			<svg
+				className={css.graph}
+				viewBox={`0 0 ${baseWidth} ${baseHeight}`}
+				preserveAspectRatio="xMidYMid meet"
+			>
+				<polygon points={points} fill="#30B94D" fillOpacity="0.2" />
 
-			<line
-				x1={startX}
-				y1={startY}
-				x2={endX}
-				y2={endY}
-				stroke="#30B94D"
-				strokeWidth={2 * svgRatio}
-				strokeLinecap="round"
-			/>
-		</svg>
+				<line
+					x1={startX}
+					y1={startY}
+					x2={endX}
+					y2={endY}
+					stroke="#30B94D"
+					strokeWidth={2}
+					strokeLinecap="round"
+				/>
+			</svg>
+		</div>
 	);
 };
 
